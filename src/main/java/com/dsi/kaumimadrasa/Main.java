daeb995ef1f8d5c53ca8366fa8525f84319300d2
syedAkash <mahmud.hasan@dsinnovators.com>
2021-11-11 16:02:52 +0600

    postgres database added

--- src/main/java/com/dsi/kaumimadrasa/Main.java
+++ src/main/java/com/dsi/kaumimadrasa/Main.java
@@ -1,7 +1,9 @@
 package com.dsi.kaumimadrasa;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,9 @@ import java.util.List;
 @RestController
 public class Main {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
@@ -19,6 +24,15 @@ public class Main {
 
     @GetMapping("/")
     public String getAll(){
+
+        String sql = "INSERT INTO students (name, email) VALUES ("
+                + "'Nam Ha Minh', 'nam@codejava.net')";
+
+        int rows = jdbcTemplate.update(sql);
+        if (rows > 0) {
+            System.out.println("A new row has been inserted.");
+        }
+
         return "hello" ;
     }
 
