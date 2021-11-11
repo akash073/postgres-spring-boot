dd773255fdc6aa4da50c783330a827235ec8025b
syedAkash <mahmud.hasan@dsinnovators.com>
2021-11-11 17:10:11 +0600

    CRUD I have  added CRUD operation

--- src/main/java/com/dsi/kaumimadrasa/controller/UserController.java
+++ src/main/java/com/dsi/kaumimadrasa/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -18,7 +19,8 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/index")
-    public String index(User user) {
+    public String index(Model model) {
+        model.addAttribute("users", userRepository.findAll());
         return "index";
     }
     
@@ -37,5 +39,32 @@ public class UserController {
         return "redirect:/index";
     }
 
-    // additional CRUD methods
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+
+        model.addAttribute("user", user);
+        return "update-user";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") long id, @Valid User user,
+                             BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            user.setId(id);
+            return "update-user";
+        }
+
+        userRepository.save(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") long id, Model model) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userRepository.delete(user);
+        return "redirect:/index";
+    }
 }
